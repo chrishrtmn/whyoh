6fff6f0a14da742fefeaaf7d30ea37ab2df02fd9
Chris Hartman <chrishartman@me.com>
2024-04-10 23:34:30 -0400

    Removing inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "WhyOH",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
         <Analytics />
       </body>
